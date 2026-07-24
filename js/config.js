af288feaa8f51815ef97da2067c77af730cf2b05
tanha-eiu <tan.ha@eiu.edu.vn>
2026-07-24 17:22:35 +0700

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -5,4 +5,4 @@
 // Dán URL Web App Google Apps Script đã deploy vào đây (dạng
 // https://script.google.com/macros/s/xxx/exec).
 // Xem hướng dẫn deploy trong file google-apps-script/Code.gs
-const SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbxcQDTWXh6kfOfWGmN4U-50VeMaZvi04WOdagAlg1kRfcvTYY6AK6yWktMZdl6rCM49nw/exec";
+const SHEET_WEBAPP_URL = "https://script.google.com/macros/s/AKfycbw-vPMPJURpCVNJGrJXzQCsVCANBdVkPgB7FLuabs5Vo9-8EBgj30z3qaQYG5X0dTqMGA/exec";
